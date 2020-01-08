c915967b983ddc42885babfcf5cf799ddbb6d11f
dam <dam@172.26.81.176>
2020-01-08 12:54:12 +0100

    terceros cambios

--- Compartir/src/compartir/Compartir.java
+++ Compartir/src/compartir/Compartir.java
@@ -17,6 +17,7 @@ public class Compartir {
     public static void main(String[] args) {
         System.out.println("5 enero");
         System.out.println("8 enero");
+        System.out.println("VERDO MOCO");
     }
     
 }
